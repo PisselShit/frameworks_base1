c17ce2a6553753509db5bd6f931436fd9712c8f7
minaripenguin <minaripenguin@users.noreply.github.com>
2024-02-02 12:33:16 +0900

    SystemUI: IslandView: Implement now playing for Island notification
    neobuddy89: Added few clean ups.
    
    Change-Id: Ic56f44fded8ee3f521738da36968e8aecfde7a95
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7186,6 +7186,11 @@ public final class Settings {
         public static final String ISLAND_NOTIFICATION = "island_notification";
 
         /**
+         * @hide
+         */
+        public static final String ISLAND_NOTIFICATION_NOW_PLAYING = "island_notification_now_playing";
+
+        /**
          * Whether to control brightness from status bar
          * 0 = 0ff, 1 = on
          * @hide
--- packages/SystemUI/src/com/android/systemui/media/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/media/NotificationMediaManager.java
@@ -15,6 +15,8 @@
  */
 package com.android.systemui.media;
 
+import static com.android.systemui.statusbar.StatusBarState.KEYGUARD;
+
 import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.app.Notification;
@@ -25,6 +27,7 @@ import android.media.session.MediaController;
 import android.media.session.MediaSession;
 import android.media.session.PlaybackState;
 import android.os.Handler;
+import android.provider.Settings;
 import android.service.notification.NotificationStats;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
@@ -37,6 +40,7 @@ import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.dump.DumpManager;
 import com.android.systemui.media.controls.domain.pipeline.MediaDataManager;
 import com.android.systemui.media.controls.shared.model.MediaData;
+import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.statusbar.NotificationPresenter;
 import com.android.systemui.statusbar.StatusBarIconView;
 import com.android.systemui.statusbar.dagger.CentralSurfacesModule;
@@ -46,6 +50,8 @@ import com.android.systemui.statusbar.notification.collection.NotificationEntry;
 import com.android.systemui.statusbar.notification.collection.notifcollection.DismissedByUserStats;
 import com.android.systemui.statusbar.notification.collection.notifcollection.NotifCollectionListener;
 import com.android.systemui.statusbar.notification.collection.render.NotificationVisibilityProvider;
+import com.android.systemui.tuner.TunerService;
+import com.android.systemui.util.NotificationUtil;
 
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -60,9 +66,14 @@ import java.util.concurrent.Executor;
  * Handles tasks and state related to media notifications. For example, there is a 'current' media
  * notification, which this class keeps track of.
  */
-public class NotificationMediaManager implements Dumpable {
+public class NotificationMediaManager implements Dumpable, TunerService.Tunable {
     private static final String TAG = "NotificationMediaManager";
     public static final boolean DEBUG_MEDIA = false;
+
+    private static final String ISLAND_NOTIFICATION =
+            "system:" + Settings.System.ISLAND_NOTIFICATION;
+    private static final String ISLAND_NOTIFICATION_NOW_PLAYING =
+            "system:" + Settings.System.ISLAND_NOTIFICATION_NOW_PLAYING;
 
     private static final HashSet<Integer> PAUSED_MEDIA_STATES = new HashSet<>();
     private static final HashSet<Integer> CONNECTING_MEDIA_STATES = new HashSet<>();
@@ -92,8 +103,16 @@ public class NotificationMediaManager implements Dumpable {
     private String mMediaNotificationKey;
     private MediaMetadata mMediaMetadata;
 
+    private final TunerService mTunerService;
+    private final NotificationUtil notifUtils;
+    private final StatusBarStateController mStatusBarStateController;
+
+    private boolean mIslandEnabled;
+    private boolean mIslandNowPlayingEnabled;
+
     @VisibleForTesting
     final MediaController.Callback mMediaListener = new MediaController.Callback() {
+
         @Override
         public void onPlaybackStateChanged(PlaybackState state) {
             super.onPlaybackStateChanged(state);
@@ -101,6 +120,16 @@ public class NotificationMediaManager implements Dumpable {
                 Log.v(TAG, "DEBUG_MEDIA: onPlaybackStateChanged: " + state);
             }
             if (state != null) {
+                if (mIslandEnabled && mIslandNowPlayingEnabled) {
+                    if (mStatusBarStateController.getState() != KEYGUARD 
+                        && !mStatusBarStateController.isDozing()
+                        && PlaybackState.STATE_PLAYING == getMediaControllerPlaybackState(mMediaController)
+                        && mMediaMetadata != null) {
+                        notifUtils.showNowPlayingNotification(mMediaMetadata);
+                    } else {
+                        notifUtils.cancelNowPlayingNotification();
+                    }
+                }
                 if (!isPlaybackActive(state.getState())) {
                     clearCurrentMediaNotification();
                 }
@@ -113,6 +142,13 @@ public class NotificationMediaManager implements Dumpable {
             super.onMetadataChanged(metadata);
             if (DEBUG_MEDIA) {
                 Log.v(TAG, "DEBUG_MEDIA: onMetadataChanged: " + metadata);
+            }
+            if (mIslandEnabled && mIslandNowPlayingEnabled) {
+                if (mStatusBarStateController.getState() != KEYGUARD 
+                        && !mStatusBarStateController.isDozing()) {
+                    notifUtils.showNowPlayingNotification(metadata);
+                }
+                notifUtils.cancelNowPlayingNotification();
             }
             mBackgroundExecutor.execute(() -> setMediaMetadata(metadata));
             dispatchUpdateMediaMetaData();
@@ -134,8 +170,9 @@ public class NotificationMediaManager implements Dumpable {
             MediaDataManager mediaDataManager,
             DumpManager dumpManager,
             @Background Executor backgroundExecutor,
-            @Main Handler handler
-    ) {
+            @Main Handler handler,
+            StatusBarStateController statusBarStateController,
+            TunerService tunerService) {
         mContext = context;
         mMediaListeners = new ArrayList<>();
         mVisibilityProvider = visibilityProvider;
@@ -144,10 +181,30 @@ public class NotificationMediaManager implements Dumpable {
         mNotifCollection = notifCollection;
         mBackgroundExecutor = backgroundExecutor;
         mHandler = handler;
+        mStatusBarStateController = statusBarStateController;
 
         setupNotifPipeline();
 
         dumpManager.registerDumpable(this);
+
+        notifUtils = new NotificationUtil(mContext);
+        mTunerService = tunerService;
+        mTunerService.addTunable(this, ISLAND_NOTIFICATION);
+        mTunerService.addTunable(this, ISLAND_NOTIFICATION_NOW_PLAYING);
+    }
+
+    @Override
+    public void onTuningChanged(String key, String newValue) {
+        switch (key) {
+            case ISLAND_NOTIFICATION:
+                mIslandEnabled = TunerService.parseIntegerSwitch(newValue, false);
+                break;
+            case ISLAND_NOTIFICATION_NOW_PLAYING:
+                mIslandNowPlayingEnabled = TunerService.parseIntegerSwitch(newValue, false);
+                break;
+            default:
+                break;
+        }
     }
 
     private void setupNotifPipeline() {
