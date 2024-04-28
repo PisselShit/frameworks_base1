744eadebfe1909a6d0a3ed2f546ea2a3e5e6e2ae
Ido Ben-Hur <idoybh2@gmail.com>
2024-04-28 13:09:16 +0300

    base: Add a dev option to unlimit screenrecord filesize [1/2]
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7712,6 +7712,14 @@ public final class Settings {
          */
         @Readable
         public static final String EDGE_SCROLLING_HAPTICS_INTENSITY = "edge_scrolling_haptics_intensity";
+        
+        /**
+         * Whether to unlimit screenrecord filesize
+         * Not for backup - dev option
+         * @hide
+         */
+        @Readable
+        public static final String UNLIMIT_SCREENRECORD = "unlimit_screenrecord";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
