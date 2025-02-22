dc13e0594e54a06e56edadd1491a23230101b128
NurKeinNeid <nurkeinneid@derpfest.org>
2025-02-22 16:14:01 +0100

    SystemUI: Add ongoing action chip toggle setting [1/2]
    Change-Id: I2083b5184d9a8233722661e42d7d5f33876fc276
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6808,6 +6808,11 @@ public final class Settings {
         public static final String NAVBAR_LONG_PRESS_GESTURE = "navbar_long_press_gesture";
 
         /**
+         * @hide
+         */
+        public static final String ONGOING_ACTION_CHIP = "ongoing_action_chip";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
