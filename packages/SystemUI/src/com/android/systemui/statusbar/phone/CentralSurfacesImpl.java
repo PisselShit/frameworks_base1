67f06d0ae770a7000e8e9b8e59ef9851da3ff6ff
Ghosuto <clash.raja10@gmail.com>
2025-11-26 06:50:37 +0000

    SystemUI: Introduce charging effects [1/2]
    Change-Id: I2c66b0f8358157a47f67bf747fac53fe5b8fd3e2
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -135,6 +135,7 @@ import com.android.systemui.demomode.DemoMode;
 import com.android.systemui.demomode.DemoModeController;
 import com.android.systemui.display.dagger.SystemUIDisplaySubcomponent;
 import com.android.systemui.edgelight.EdgeLightViewController;
+import com.android.systemui.charging.ChargingAnimationViewController;
 import com.android.systemui.nowplaying.NowPlayingViewController;
 import com.android.systemui.emergency.EmergencyGesture;
 import com.android.systemui.emergency.EmergencyGestureModule.EmergencyGestureIntentFactory;
@@ -484,6 +485,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
     private final PulseViewController mPulseViewController;
     private final EdgeLightViewController mEdgeLightViewController;
     private final NowPlayingViewController mNowPlayingViewController;
+    private final ChargingAnimationViewController mChargingAnimationViewController;
 
     private final GameSpaceManager mGameSpaceManager;
 
@@ -756,6 +758,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
             PulseViewController pulseViewController,
             EdgeLightViewController edgeLightViewController,
             NowPlayingViewController nowPlayingViewController,
+            ChargingAnimationViewController chargingAnimationViewController,
             BurnInProtectionController burnInProtectionController
     ) {
         mContext = context;
@@ -909,6 +912,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         mPulseViewController = pulseViewController;
         mEdgeLightViewController = edgeLightViewController;
         mNowPlayingViewController = nowPlayingViewController;
+        mChargingAnimationViewController = chargingAnimationViewController;
     }
 
     private void initBubbles(Bubbles bubbles) {
@@ -1153,6 +1157,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         getNotifContainerParentView().addView(mPulseViewController.getPulseView(), 1);
         getNotifContainerParentView().addView(mEdgeLightViewController.getEdgeLightView(), 2);
         getNotifContainerParentView().addView(mNowPlayingViewController.getNowPlayingView(), 3);
+        getNotifContainerParentView().addView(mChargingAnimationViewController.getChargingView(), 4);
     }
 
     private ViewGroup getNotifContainerParentView() {
