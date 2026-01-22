2615fc65a1b31bea447d3ca3cd2254534c2aaea8
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-01-22 19:20:17 +0100

    SystemUI: Add notification row transluency toggle
    Change-Id: I4ea5385caf72336bd8fbc87f85e3e00e85d189d2
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14563,6 +14563,12 @@ public final class Settings {
         public static final String HIDE_DEVELOPER_STATUS = "hide_developer_status";
 
         /**
+         * Translucent notifications
+         * @hide
+         */
+        public static final String NOTIFICATION_ROW_TRANSPARENCY = "notification_row_transparency";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
