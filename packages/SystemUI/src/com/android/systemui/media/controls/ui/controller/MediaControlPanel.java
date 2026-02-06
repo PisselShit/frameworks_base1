crdroidandroid/android_frameworks_base

MediaControlPanel: Fix media playback crash on chromium based browser
Log:
11-25 02:02:28.144 20301 20327 W MediaControlPanel: Cannot find icon for package org.mozilla.firefox
11-25 02:02:28.144 20301 20327 W MediaControlPanel: android.content.pm.PackageManager$NameNotFoundException: org.mozilla.firefox
11-25 02:02:28.144 20301 20327 W MediaControlPanel: 	at android.app.ApplicationPackageManager.getApplicationInfoAsUser(ApplicationPackageManager.java:553)
11-25 02:02:28.144 20301 20327 W MediaControlPanel: 	at android.app.ApplicationPackageManager.getApplicationInfo(ApplicationPackageManager.java:536)
11-25 02:02:28.144 20301 20327 W MediaControlPanel: 	at android.app.ApplicationPackageManager.getApplicationInfo(ApplicationPackageManager.java:530)
11-25 02:02:28.144 20301 20327 W MediaControlPanel: 	at android.app.ApplicationPackageManager.getApplicationIcon(ApplicationPackageManager.java:2070)
11-25 02:02:28.144 20301 20327 W MediaControlPanel: 	at com.android.systemui.media.controls.ui.controller.MediaControlPanel$$ExternalSyntheticLambda16.run(go/retraceme f7d95441be99f4dfeede6103004cee1d1e11bfe8ca456da4ee1cf3dd9b3f64eb:86)
11-25 02:02:28.144 20301 20327 W MediaControlPanel: 	at android.os.Handler.handleCallback(Handler.java:995)
11-25 02:02:28.144 20301 20327 W MediaControlPanel: 	at android.os.Handler.dispatchMessage(Handler.java:103)
11-25 02:02:28.144 20301 20327 W MediaControlPanel: 	at android.os.Looper.loopOnce(Looper.java:248)
11-25 02:02:28.144 20301 20327 W MediaControlPanel: 	at android.os.Looper.loop(Looper.java:338)
11-25 02:02:28.144 20301 20327 W MediaControlPanel: 	at android.os.HandlerThread.run(HandlerThread.java:85)

11-25 02:02:28.158 20301 20301 D AndroidRuntime: Shutting down VM
11-25 02:02:28.158 20301 20301 E AndroidRuntime: FATAL EXCEPTION: main
11-25 02:02:28.158 20301 20301 E AndroidRuntime: Process: com.android.systemui, PID: 20301
11-25 02:02:28.158 20301 20301 E AndroidRuntime: java.lang.NullPointerException: Attempt to read from fie

packages/SystemUI/src/com/android/systemui/media/controls/ui/controller/MediaControlPanel.java
@@ -870,6 +870,8 @@ public class MediaControlPanel {
                             darkTheme, Style.CONTENT);
                 } catch (PackageManager.NameNotFoundException e) {
                     Log.w(TAG, "Cannot find icon for package " + data.getPackageName(), e);
+                    mutableColorScheme = new ColorScheme(ColorScheme.GOOGLE_BLUE,
+                            darkTheme, Style.CONTENT);
                 }
             }
 

PisselShit/frameworks_base1

Signed-off-by: Ghosuto <clash.raja10@gmail.com>

packages/SystemUI/src/com/android/systemui/media/controls/ui/controller/MediaControlPanel.java
@@ -918,6 +918,8 @@ public class MediaControlPanel {
                             darkTheme, ThemeStyle.CONTENT);
                 } catch (PackageManager.NameNotFoundException e) {
                     Log.w(TAG, "Cannot find icon for package " + data.getPackageName(), e);
+                    mutableColorScheme = new ColorScheme(ColorScheme.GOOGLE_BLUE,
+                            darkTheme, ThemeStyle.CONTENT);
                 }
             }
 