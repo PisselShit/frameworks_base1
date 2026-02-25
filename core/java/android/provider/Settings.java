97bd95afbad69b60077244a4999f747d45afd663
Pranav Vashi <neobuddy89@gmail.com>
2026-02-25 21:16:11 +0530

    SystemUI: Add QS tile gradient customization
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7870,6 +7870,12 @@ public final class Settings {
          * @hide
          */
         public static final String ONGOING_COMPACT_MODE = "ongoing_compact_mode";
+        
+        /**
+         * Gradient on QS tiles
+         * @hide
+         */
+        public static final String QS_TILE_GRADIENT = "qs_tile_gradient";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
