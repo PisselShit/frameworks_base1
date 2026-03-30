F5LAYD/frameworks_base

SystemUI: Added Lunaris volume panel style [1/2]
- Axion vol. panel theme https://github.com/Lunaris-AOSP/frameworks_base/commit/11f91c16ff1e5b0d06a02a8ace45b75436e60aca

- Design inspried from Subratam6296 Redesign style
- Adapt for compose

Signed-off-by: Ghosuto <clash.raja10@gmail.com>

packages/SystemUI/src/com/android/systemui/axion/volume/AxionVolumeDialog.kt
@@ -151,7 +151,7 @@ class AxionVolumeDialog @Inject constructor(
                             .padding(12.dp),
                         contentAlignment = Alignment.Center
                     ) {
-                        AxionVolumeDialogContent(viewModel)
+                        AxionVolumeStyleDispatcher(viewModel)
                     }
                 }
             }

PisselShit/frameworks_base1

packages/SystemUI/src/com/android/systemui/axion/volume/AxionVolumeDialog.kt
@@ -151,7 +151,7 @@ class AxionVolumeDialog @Inject constructor(
                             .padding(12.dp),
                         contentAlignment = Alignment.Center
                     ) {
-                        AxionVolumeDialogContent(viewModel)
+                        VolumeStyleDispatcher(viewModel)
                     }
                 }
             }